09c595003b4a235429b89f4ef01501a9e92989f4
gilzilberfeld <gil.zilberfeld@gmail.com>
2024-03-25 16:55:28 +0200

    testable pom

---
+++ tests/d08.testablepom/consts.ts
@@ -0,0 +1,12 @@
+// URLs
+export const THANK_YOU_PAGE = "/thankyou";
+export const REGISTRATION_PAGE = "/";
+
+// APIs
+export const API_GET_ALL = "/api/all";
+
+export const ANY_FIRST_NAME = "gil";
+export const ANY_LAST_NAME = "zilberfeld";
+export const ANY_VALID_EMAIL = "gil@testingil.com";
+export const ANY_INPUT = "a";
+
